60bb93beb9449da66e1edc14a7036eb5be05c8ef
Lee beu ni el <zpftr@naver.com>
2022-03-12 13:30:37 +0900

    removed hello.java added menumanager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,8 +1,12 @@
 
-public class hello {
+public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("1. Add Students");
+		System.out.println("2. Delete Students");
+		System.out.println("3. Edit Students");
+		System.out.println("4. View Students");
+		System.out.println("5. Exit");
 
 	}
 
