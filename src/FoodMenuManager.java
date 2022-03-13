a48756dacff3dd7b089383c5bd6f5ba2afc309b8
Lee beu ni el <zpftr@naver.com>
2022-03-13 19:31:10 +0900

    제목 수정

---
+++ src/FoodMenuManager.java
@@ -0,0 +1,13 @@
+
+public class FoodMenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Foods");
+		System.out.println("2. Delete Foods");
+		System.out.println("3. Edit Foods");
+		System.out.println("4. View Foods");
+		System.out.println("5. Exit");
+
+	}
+
+}
