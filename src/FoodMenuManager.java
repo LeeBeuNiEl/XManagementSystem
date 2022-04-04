ea046f5c4d65b5e2c501052e2c7f9066d4fbf86e
Lee beu ni el <zpftr@naver.com>
2022-04-05 01:28:10 +0900

    add class, method, collection

--- src/FoodMenuManager.java
+++ src/FoodMenuManager.java
@@ -1,14 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FoodMenuManager{
-	Food food;
+	ArrayList<Food> foods = new ArrayList<Food>();
 	Scanner input;
 	FoodMenuManager(Scanner input){
 		this.input = input;
 	}
 
 	public void addFoods() {
-		food = new Food();
+		Food food = new Food();
 		System.out.print("Food Number : ");
 		food.number = input.nextInt();
 		System.out.print("Food Name : ");
@@ -23,29 +24,70 @@ public class FoodMenuManager{
 	public void deleteFoods() {
 		System.out.print("Food Number : ");
 		int foodnumber = input.nextInt();
-		if(food == null) {
+		int index = -1;
+		for (int i =0; i < foods.size(); i++){
+			if(foods.get(i).number == foodnumber) {
+				index = i;
+				break;
+			}
+		}
+		if (index >= 0) {
+			foods.remove(index);
+			Food.numFoodsRegistered--;
+			System.out.println("the food" + foodnumber + "is deleted");
+		}
+		else {
 			System.out.println("the food has not been registered");
 			return;
-		}
-		if (food.number == foodnumber) {
-			food = null;
-			System.out.println("the food is deleted");
 		}
 	}
 	
 	public void editFoods() {
 		System.out.print("Food Number : ");
 		int foodnumber = input.nextInt();
-		if (food.number == foodnumber) {
-			System.out.println("the food to be edited is " + foodnumber);
+		for (int i = 0; i < foods.size(); i++) {
+			Food food = foods.get(i);
+			if (food.number == foodnumber) {
+				int num = -1;
+				while (num != 5) {
+					System.out.println("***********************");
+					System.out.println("1. edit number");
+					System.out.println("2. edit food name");
+					System.out.println("3. edit adrress");
+					System.out.println("4. edit telephone number");
+					System.out.println("5. Exit");
+					System.out.print("Slect one number between 1-5 : ");
+					num = input.nextInt();
+					
+					if (num == 1) {
+						System.out.print("Food Number : ");
+						food.number = input.nextInt();
+					}
+					else if (num == 2) {
+						System.out.print("Food Name : ");
+						food.name = input.nextLine();
+					}
+					else if (num == 3) {
+						System.out.print("Store Address : ");
+						food.address = input.nextLine();
+					}
+					else if (num == 4) {
+						System.out.print("Store Telephone : ");
+						food.telephone = input.nextLine();
+					}
+					else {
+						continue;
+					}
+				}
+				break;
+			}
 		}
 	}
 	
 	public void viewFoods() {
-		System.out.print("Food Number : ");
-		int foodnumber = input.nextInt();
-		if (food.number == foodnumber) {
-			food.printInfo();
+		System.out.print("# of registered foods : " + Food.numFoodsRegistered);
+		for (int i = 0; i < foods.size(); i++) {
+			foods.get(i).printInfo();
 		}
 	}
 }
